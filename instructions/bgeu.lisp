edd101c42aad20214741f91632c021d4782da316
Quang Dao <quangdao@umich.edu>
2024-09-23 23:48:08 -0400

    32-bit and 64-bit versions of `sltu` and `bgeu`

--- instructions/bgeu.lisp
+++ instructions/bgeu.lisp
@@ -7,8 +7,7 @@
 (include-book "centaur/bitops/ihsext-basics" :dir :system)
 (include-book "centaur/bitops/fast-logext" :dir :system)
 
-(include-book "../ltu")
-(include-book "../eq")
+(include-book "sltu")
 
 (include-book "ihs/logops-lemmas" :dir :system)
 (include-book "centaur/bitops/part-select" :DIR :SYSTEM)
@@ -16,7 +15,7 @@
 
 ;; 32-BIT VERSION
 
-(define sltu-semantics-32 ((x (unsigned-byte-p 32 x)) (y (unsigned-byte-p 32 y)))
+(define bgeu-semantics-32 ((x (unsigned-byte-p 32 x)) (y (unsigned-byte-p 32 y)))
   (b* (((unless (unsigned-byte-p 32 x)) 0)
        ((unless (unsigned-byte-p 32 y)) 0)
        ;; CHUNK
@@ -38,13 +37,13 @@
        (w2   (if (= x8-2 y8-2) 1 0))
        (?w3  (if (= x8-3 y8-3) 1 0))) ;; ignore w3
       ;; COMBINE
-      (+    z0
-         (* z1 w0)
-         (* z2 w0 w1)
-	 (* z3 w0 w1 w2))))
+      (- 1 (+ z0
+        (* z1 w0)
+        (* z2 w0 w1)
+        (* z3 w0 w1 w2)))))
 
-;; Define sltu with lookups to subtables
-(define sltu-32 ((x (unsigned-byte-p 32 x)) (y (unsigned-byte-p 32 y)))
+;; Define bgeu with lookups to subtables
+(define bgeu-32 ((x (unsigned-byte-p 32 x)) (y (unsigned-byte-p 32 y)))
   :verify-guards nil
   (b* (((unless (unsigned-byte-p 32 x)) 0)
        ((unless (unsigned-byte-p 32 y)) 0)
@@ -71,39 +70,34 @@
        (w2   (lookup x8-2 y8-2  eq-subtable))
        (?w3  (lookup x8-3 y8-3  eq-subtable))) ;; ignore w3
       ;; COMBINE
-      (+    z0
-         (* z1 w0)
-         (* z2 w0 w1)
-	 (* z3 w0 w1 w2))))
+      (- 1 (+ z0
+        (* z1 w0)
+        (* z2 w0 w1)
+	      (* z3 w0 w1 w2)))))
 
 (in-theory (disable (:EXECUTABLE-COUNTERPART EXPT)))
 
-;; (defthm sltu-semantics-32-when-0
-;;  (implies (or (not (unsigned-byte-p 32 x)) (not (unsigned-byte-p 32 y)))
-;; 	  (equal (sltu-semantics-32 x y) 0))
-;;  :hints (("Goal" :use ((:instance sltu-semantics-32)))))
-
-(defthm sltu-32-sltu-semantics-32-equiv
- (equal (sltu-32 x y)
-	(sltu-semantics-32 x y))
- :hints (("goal" :in-theory (e/d (sltu-32 sltu-semantics-32) ((:e create-x-indices))))))
+(defthm bgeu-32-bgeu-semantics-32-equiv
+ (equal (bgeu-32 x y)
+	(bgeu-semantics-32 x y))
+ :hints (("goal" :in-theory (e/d (bgeu-32 bgeu-semantics-32) ((:e create-x-indices))))))
 	         
-;; SEMANTIC CORRECTNESS OF SLTU
-(gl::def-gl-thm sltu-semantics-32-correctness
+;; SEMANTIC CORRECTNESS OF BGEU
+(gl::def-gl-thm bgeu-semantics-32-correctness
  :hyp (and (unsigned-byte-p 32 x) (unsigned-byte-p 32 y))
- :concl (equal (sltu-semantics-32 x y)
-	       (if (< x y) 1 0))
+ :concl (equal (bgeu-semantics-32 x y)
+	       (if (>= x y) 1 0))
  :g-bindings (gl::auto-bindings (:mix (:nat x 32) (:nat y 32))))
 
-;; CORRECTNESS OF SLTU
-(defthm sltu-32-correctness
+;; CORRECTNESS OF BGEU
+(defthm bgeu-32-correctness
  (implies (and (unsigned-byte-p 32 x) (unsigned-byte-p 32 y))
-          (equal (sltu-semantics-32 x y) (if (< x y) 1 0))))
+          (equal (bgeu-semantics-32 x y) (if (>= x y) 1 0))))
 
 
 ;; 64-BIT VERSION
 
-(define sltu-semantics-64 ((x (unsigned-byte-p 64 x)) (y (unsigned-byte-p 64 y)))
+(define bgeu-semantics-64 ((x (unsigned-byte-p 64 x)) (y (unsigned-byte-p 64 y)))
   (b* (((unless (unsigned-byte-p 64 x)) 0)
        ((unless (unsigned-byte-p 64 y)) 0)
        ;; CHUNK
@@ -141,17 +135,17 @@
        (w6   (if (= x8-6 y8-6) 1 0))
        (?w7  (if (= x8-7 y8-7) 1 0))) ;; ignore w7
        ;; COMBINE
-       (+    z0
+       (- 1 (+ z0
         (* z1 w0)
         (* z2 w0 w1)
         (* z3 w0 w1 w2)
         (* z4 w0 w1 w2 w3)
         (* z5 w0 w1 w2 w3 w4)
         (* z6 w0 w1 w2 w3 w4 w5)
-        (* z7 w0 w1 w2 w3 w4 w5 w6))))
+        (* z7 w0 w1 w2 w3 w4 w5 w6)))))
 
-;; Define sltu with lookups to subtables
-(define sltu-64 ((x (unsigned-byte-p 64 x)) (y (unsigned-byte-p 64 y)))
+;; Define bgeu with lookups to subtables
+(define bgeu-64 ((x (unsigned-byte-p 64 x)) (y (unsigned-byte-p 64 y)))
   :verify-guards nil
   (b* (((unless (unsigned-byte-p 64 x)) 0)
        ((unless (unsigned-byte-p 64 y)) 0)
@@ -194,35 +188,30 @@
        (w6   (lookup x8-6 y8-6  eq-subtable))
        (?w7  (lookup x8-7 y8-7  eq-subtable))) ;; ignore w7
       ;; COMBINE
-      (+    z0
+      (- 1 (+ z0
         (* z1 w0)
         (* z2 w0 w1)
         (* z3 w0 w1 w2)
         (* z4 w0 w1 w2 w3)
         (* z5 w0 w1 w2 w3 w4)
         (* z6 w0 w1 w2 w3 w4 w5)
-        (* z7 w0 w1 w2 w3 w4 w5 w6))))
+        (* z7 w0 w1 w2 w3 w4 w5 w6)))))
 
 (in-theory (disable (:EXECUTABLE-COUNTERPART EXPT)))
 
-;; (defthm sltu-semantics-64-when-0
-;;  (implies (or (not (unsigned-byte-p 64 x)) (not (unsigned-byte-p 64 y)))
-;; 	  (equal (sltu-semantics-64 x y) 0))
-;;  :hints (("Goal" :use ((:instance sltu-semantics-64)))))
-
-(defthm sltu-64-sltu-semantics-64-equiv
- (equal (sltu-64 x y)
-	(sltu-semantics-64 x y))
- :hints (("goal" :in-theory (e/d (sltu-64 sltu-semantics-64) ((:e create-x-indices))))))
+(defthm bgeu-64-bgeu-semantics-64-equiv
+ (equal (bgeu-64 x y)
+	(bgeu-semantics-64 x y))
+ :hints (("goal" :in-theory (e/d (bgeu-64 bgeu-semantics-64) ((:e create-x-indices))))))
 
 ;; SEMANTIC CORRECTNESS OF SLTU
-(gl::def-gl-thm sltu-semantics-64-correctness
+(gl::def-gl-thm bgeu-semantics-64-correctness
  :hyp (and (unsigned-byte-p 64 x) (unsigned-byte-p 64 y))
- :concl (equal (sltu-semantics-64 x y)
-	       (if (< x y) 1 0))
+ :concl (equal (bgeu-semantics-64 x y)
+	       (if (>= x y) 1 0))
  :g-bindings (gl::auto-bindings (:mix (:nat x 64) (:nat y 64))))
 
 ;; CORRECTNESS OF SLTU
-(defthm sltu-64-correctness
+(defthm bgeu-64-correctness
  (implies (and (unsigned-byte-p 64 x) (unsigned-byte-p 64 y))
-          (equal (sltu-semantics-64 x y) (if (< x y) 1 0))))
+          (equal (bgeu-semantics-64 x y) (if (>= x y) 1 0))))
