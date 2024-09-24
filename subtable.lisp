6e9a20b37e7145ea03514ed74d3bc5308b2f5628
Quang Dao <quangdao@umich.edu>
2024-09-24 11:10:04 -0400

    added documentation to `subtable

--- subtable.lisp
+++ subtable.lisp
@@ -3,22 +3,37 @@
 (include-book "std/util/bstar" :dir :system)
 (include-book "std/util/define" :dir :system)
 
+;; Check if `x` is a `cons` (i.e. a pair of values). If yes, return `x`. If no, return `(cons x nil)`, i.e. `x` as the first value and `nil` as the second value.
 (defun cons-fix (x)
  (if (consp x) x (cons x nil)))
 (verify-guards cons-fix)
 
+;; Define a new fixed type `cons`, using the `fty` (finite types) library, with the following properties:
 (fty::deffixtype cons
+ ;; The predicate `consp` is used to determine if a value is a `cons`.
  :pred consp
+ ;; The fixer function `cons-fix` is used to convert a value to a `cons`.
  :fix  cons-fix
+ ;; The equivalence relation `cons-equiv` is used to determine if two values are equivalent.
  :equiv cons-equiv
+ ;; The definition of the type is enabled.
  :define t
+ ;; The forward definition of the type is enabled (i.e. automatically apply type properties when proving theorems)
  :forward t)
 
+;; Define a new association list type called `subtable`, using the `fty` (finite types) library, with the following properties:
 (fty::defalist subtable
+ ;; The key type of the subtable is `cons`.
  :key-type cons
+ ;; The value type of the subtable is `nat`.
  :val-type nat
+ ;; The subtable is a true list (i.e. `nil` as the last value).
  :true-listp t)
 
+;; Generate a list of `cons` values, where the first element is `fixed-x` and the second element is a natural number `i` that ranges from `y-hi` to `0`.
+;; If `y-hi` or `fixed-x` is not a natural number, return `nil`.
+;; If `y-hi` is `0`, return a list containing the `cons` value `(cons fixed-x y-hi)`.
+;; Otherwise, return a list containing the `cons` value `(cons fixed-x y-hi)` appended to the result of recursively calling `create-y-indices` with `fixed-x` and `(y-hi - 1)`.
 (defun create-y-indices (fixed-x y-hi)
  (if (or (not (natp y-hi)) (not (natp fixed-x)))
      nil
@@ -27,10 +42,15 @@
          (cons (cons fixed-x y-hi)
                (create-y-indices fixed-x (1- y-hi))))))
 
+;; For any natural numbers `x`, `y-hi`, and `i`, if `i` is less than or equal to `y-hi`, then `(cons x i)` is a member of the list returned by `create-y-indices` with `x` and `y-hi`.
 (defthmd create-y-indices-correctness
  (implies (and (natp x) (natp y-hi) (natp i) (<= i y-hi))
           (member (cons x i) (create-y-indices x y-hi))))
 
+;; Generate a list of `cons` values, where pairs are of the form `(cons i j)` with `i` ranging from `x-hi` to `0` and `j` ranging from `y-hi` to `0`.
+;; If `x-hi` or `y-hi` is not a natural number, return `nil`.
+;; If `x-hi` is `0`, return the list returned by `create-y-indices` with `x-hi` and `y-hi`.
+;; Otherwise, return the list returned by `create-y-indices` with `x-hi` and `y-hi` appended to the result of recursively calling `create-x-indices` with `(x-hi - 1)` and `y-hi`.
 (defun create-x-indices (x-hi y-hi)
  (if (or (not (natp x-hi)) (not (natp y-hi)))
      nil
@@ -38,11 +58,15 @@
          (create-y-indices x-hi y-hi)
          (append (create-y-indices x-hi y-hi)
                  (create-x-indices (1- x-hi) y-hi)))))
+
+;; `create-x-indices` returns a valid association list
 (defthm alistp-of-create-x-indices
  (alistp (create-x-indices x-hi y-hi)))
+
 (verify-guards create-y-indices)
 (verify-guards create-x-indices)
 
+;; For any natural numbers `x`, `y-hi`, `i`, and `j`, if `i` is less than or equal to `x-hi` and `j` is less than or equal to `y-hi`, then `(cons i j)` is a member of the list returned by `create-x-indices` with `x-hi` and `y-hi`.
 (defthm create-x-indices-correctness
  (implies (and (natp x-hi) 
                (natp y-hi) 
@@ -52,6 +76,8 @@
                (<= j y-hi) )
           (member (cons i j) (create-x-indices x-hi y-hi))))
 
+;; Lookup the value associated with the key `(cons x y)` in the association list `table`.
+;; `assoc-equal` is a function that returns the `cons` pair associated with the key `(cons x y)` in the association list `table`. We then return the value (i.e. second element) of this pair.
 (defund lookup (x y table)
  (cdr (assoc-equal (cons x y) table)))
 (verify-guards lookup)
