77a16551ad96fee5386fab05caa8265b9970d359
Carl Kwan <>
2024-10-03 05:01:32 -0500

    Cleaner ltu-w version for paper

--- subtables/ltu.lisp
+++ subtables/ltu.lisp
@@ -16,27 +16,17 @@
 ;;			        ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define b-ltu ((x0 bitp) (y0 bitp))
-  :returns (lt bitp)
-  :enabled t
-  (b-and (b-xor 1 x0) y0)
-  ///
-  (defthm b-ltu-<-equiv
-    (implies (and (bitp x) (bitp y))
-	     (equal (b-ltu x y)
-		    (if (< x y) 1 0)))))
-
-(define ltu-i ((x :type unsigned-byte)
-	       (y :type unsigned-byte)
-               (i natp))
-  :returns (lti bitp)
-  (b-and (b-ltu (logbit i x) (logbit i y))
-	 (eqw (logtail i x) (logtail i y))))
-
-(define ltu-0 ((x :type unsigned-byte) (y :type unsigned-byte))
-  :enabled t
-  (b-and (b-ltu (logbit 1 x) (logbit 1 y))
-	 (eqw (logtail 1 x) (logtail 1 y))))
+;(define ltu-i ((x :type unsigned-byte)
+;	       (y :type unsigned-byte)
+;               (i natp))
+;  :returns (lti bitp)
+;  (b-and (b-ltu (logbit i x) (logbit i y))
+;	 (eqw (logtail i x) (logtail i y))))
+;
+;(define ltu-0 ((x :type unsigned-byte) (y :type unsigned-byte))
+;  :enabled t
+;  (b-and (b-ltu (logbit 1 x) (logbit 1 y))
+;	 (eqw (logtail 1 x) (logtail 1 y))))
 
 (local
   (defthm loghead-logcar-equiv
@@ -50,24 +40,54 @@
 	    (equal (logcdr x) (logtail 1 x)))
    :hints (("Goal" :in-theory (enable logcdr logtail)))))
 
+
+(define b-ltu-w ((x0 bitp) (y0 bitp))
+  :returns (lt bitp)
+  (b-and (b-xor 1 x0) y0)
+  ///
+  (defthm b-ltu-w-<-equiv
+    (implies (and (bitp x) (bitp y))
+	     (equal (b-ltu-w x y)
+		    (if (< x y) 1 0)))))
+
+
+
+
+
+;(local
+;  (defthm integer-length-lemma
+;   (implies (equal x 0)
+;	    (equal (integer-length x) 0))
+;   :hints (("Goal" :in-theory (enable bitp integer-length)))))
+
+
+;; Compute the MLE for LTU
 (define ltu-w ((x :type unsigned-byte) (y :type unsigned-byte))
   :measure (max (integer-length x) (integer-length y))
-  (b* (((unless (and (natp x) (natp y))) 0)
-       ((if (and (zerop (integer-length x)) (zerop (integer-length y)))) 0)
+  (b* (;; Edge case
+       ((unless (and (natp x) (natp y)))      0)
+       ;; Base case
+       ((if (and (zerop (integer-length x)) 
+		 (zerop (integer-length y)))) 0)
+       ;; Bindings for x and y
        (x-0    (logcar x))
        (y-0    (logcar y))
        (x-rest (logcdr x))
        (y-rest (logcdr y))
+       ;; Current summand
        (ltu-0  (b-and (b-and (b-xor 1 x-0) y-0)
 	 	      (eq-w x-rest y-rest))))
+      ;; Recursive case
       (b-xor ltu-0 (ltu-w x-rest y-rest))))
 
-(gl::def-gl-thm ltu-<-equiv-gl
+;; Theorem which states LTU is equivalent to < for 32-bit case
+(gl::def-gl-thm ltu-w-<-equiv-gl-32
   :hyp   (and (unsigned-byte-p 32 x)
               (unsigned-byte-p 32 y))
-  :concl (equal (ltu-w x y)
-      	  	(if (< x y) 1 0))
+  :concl (equal (ltu-w x y) (if (< x y) 1 0))
   :g-bindings (gl::auto-bindings (:mix (:nat x 32) (:nat y 32))))
+
+
 
 (define ltuwc ((x :type unsigned-byte) (y :type unsigned-byte) (wc posp))
   :returns (ltu? bitp) ; :hyp :guard)
