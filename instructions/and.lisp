7e04cefd3db6396faf9edbe5540951d65f43f627
Carl Kwan <>
2024-10-08 09:23:26 -0500

    Cleaning and.lisp

--- instructions/and.lisp
+++ instructions/and.lisp
@@ -1,17 +1,13 @@
 (in-package "ACL2")
 (include-book "std/util/bstar" :dir :system)
 (include-book "std/util/define" :dir :system)
-(include-book "centaur/gl/gl" :dir :system)
 (include-book "arithmetic/top" :dir :system)
-
+(include-book "centaur/gl/gl" :dir :system)
 (include-book "centaur/bitops/ihsext-basics" :dir :system)
-(include-book "centaur/bitops/fast-logext" :dir :system)
-
-(include-book "../subtables/and")
-
-(include-book "ihs/logops-lemmas" :dir :system)
 (include-book "centaur/bitops/part-select" :DIR :SYSTEM)
 (include-book "centaur/bitops/merge" :DIR :SYSTEM)
+
+(include-book "../subtables/and")
 
 (local
  (gl::def-gl-thm logtail-24-lemma
@@ -24,7 +20,7 @@
 (define and-semantics-32 ((x (unsigned-byte-p 32 x)) (y (unsigned-byte-p 32 y)))
   (b* (((unless (unsigned-byte-p 32 x)) 0)
        ((unless (unsigned-byte-p 32 y)) 0)
-       ;; CHUNK
+       ;; Chunk
        (x8-3 (part-select x :low  0 :width 8))
        (x8-2 (part-select x :low  8 :width 8))
        (x8-1 (part-select x :low 16 :width 8))
@@ -33,19 +29,19 @@
        (y8-2 (part-select y :low  8 :width 8))
        (y8-1 (part-select y :low 16 :width 8))
        (y8-0 (part-select y :low 24 :width 8))
-       ;; LOOKUP SEMANTICS
+       ;; Lookup semantics
        (w0   (logand x8-0 y8-0))
        (w1   (logand x8-1 y8-1))
        (w2   (logand x8-2 y8-2))
        (w3   (logand x8-3 y8-3)))
-      ;; COMBINE
+      ;; Combine
       (merge-4-u8s w0 w1 w2 w3)))
 
 (define and-32 ((x (unsigned-byte-p 32 x)) (y (unsigned-byte-p 32 y)))
   :verify-guards nil
   (b* (((unless (unsigned-byte-p 32 x)) 0)
        ((unless (unsigned-byte-p 32 y)) 0)
-       ;; CHUNK
+       ;; Chunk
        (x8-3 (part-select x :low  0 :width 8))
        (x8-2 (part-select x :low  8 :width 8))
        (x8-1 (part-select x :low 16 :width 8))
@@ -54,22 +50,22 @@
        (y8-2 (part-select y :low  8 :width 8))
        (y8-1 (part-select y :low 16 :width 8))
        (y8-0 (part-select y :low 24 :width 8))
-       ;; MATERIALIZE SUBTABLES 
+       ;; Materialize subtables 
        (indices      (create-tuple-indices (1- (expt 2 8)) (1- (expt 2 8))))
        (and-subtable  (materialize-and-subtable  indices))
-       ;; LOOKUPS
+       ;; Perform lookups
        (w0   (tuple-lookup x8-0 y8-0 and-subtable))
        (w1   (tuple-lookup x8-1 y8-1 and-subtable))
        (w2   (tuple-lookup x8-2 y8-2 and-subtable))
        (w3   (tuple-lookup x8-3 y8-3 and-subtable)))
-      ;; COMBINE
+      ;; Combine
       (merge-4-u8s w0 w1 w2 w3)))
 
 (defthm and-32-and-semantics-32-equiv
  (equal (and-32 x y) (and-semantics-32 x y))
  :hints (("Goal" :in-theory (e/d (and-32 and-semantics-32) ((:e create-tuple-indices))))))
 
-;; SEMANTIC CORRECTNESS OF AND
+;; Semantic correctness of and
 (gl::def-gl-thm and-semantics-32-correctness
  :hyp (and (unsigned-byte-p 32 x) (unsigned-byte-p 32 y))
  :concl (equal (and-semantics-32 x y) (logand x y))
@@ -86,7 +82,7 @@
   :verify-guards nil
   (b* (((unless (unsigned-byte-p 64 x)) 0)
        ((unless (unsigned-byte-p 64 y)) 0)
-       ;; CHUNK
+       ;; Chunk
        (x8-7 (part-select x :low  0 :width 8))
        (x8-6 (part-select x :low  8 :width 8))
        (x8-5 (part-select x :low 16 :width 8))
@@ -103,7 +99,7 @@
        (y8-2 (part-select y :low 40 :width 8))
        (y8-1 (part-select y :low 48 :width 8))
        (y8-0 (part-select y :low 56 :width 8))
-       ;; LOOKUP SEMANTICS
+       ;; Lookup semantics
        (w0   (logand x8-0 y8-0))
        (w1   (logand x8-1 y8-1))
        (w2   (logand x8-2 y8-2))
@@ -112,14 +108,14 @@
        (w5   (logand x8-5 y8-5))
        (w6   (logand x8-6 y8-6))
        (w7   (logand x8-7 y8-7)))
-      ;; COMBINE
+      ;; Combine
       (merge-8-u8s w0 w1 w2 w3 w4 w5 w6 w7)))
 
 (define and-64 ((x (unsigned-byte-p 64 x)) (y (unsigned-byte-p 64 y)))
   :verify-guards nil
   (b* (((unless (unsigned-byte-p 64 x)) 0)
        ((unless (unsigned-byte-p 64 y)) 0)
-       ;; CHUNK
+       ;; Chunk
        (x8-7 (part-select x :low  0 :width 8))
        (x8-6 (part-select x :low  8 :width 8))
        (x8-5 (part-select x :low 16 :width 8))
@@ -136,10 +132,10 @@
        (y8-2 (part-select y :low 40 :width 8))
        (y8-1 (part-select y :low 48 :width 8))
        (y8-0 (part-select y :low 56 :width 8))
-       ;; MATERIALIZE SUBTABLES 
-       (indices            (create-tuple-indices (expt 2 8) (expt 2 8)))
-       (and-subtable        (materialize-and-subtable  indices))
-       ;; LOOKUPS
+       ;; Materialize subtables 
+       (indices      (create-tuple-indices (expt 2 8) (expt 2 8)))
+       (and-subtable (materialize-and-subtable  indices))
+       ;; Perform lookups
        (w0   (tuple-lookup x8-0 y8-0 and-subtable))
        (w1   (tuple-lookup x8-1 y8-1 and-subtable))
        (w2   (tuple-lookup x8-2 y8-2 and-subtable))
@@ -156,7 +152,7 @@
 	(and-semantics-64 x y))
  :hints (("Goal" :in-theory (e/d (and-64 and-semantics-64) ((:e expt))))))
 
-;; SEMANTIC CORRECTNESS OF AND
+;; Semantic correctness of and
 (gl::def-gl-thm and-semantics-64-correctness
  :hyp (and (unsigned-byte-p 64 x) (unsigned-byte-p 64 y))
  :concl (equal (and-semantics-64 x y) (logand x y))
